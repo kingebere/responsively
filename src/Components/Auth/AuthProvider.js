dae1df78c9e68811225125da5fc286a416d197c8
Dike  Goodluck <Gudoski30@gmail.com>
2022-04-12 03:54:43 +0100

    remove config

--- src/Components/Auth/AuthProvider.js
+++ src/Components/Auth/AuthProvider.js
@@ -8,7 +8,7 @@ export const AuthProvider = ({ children }) => {
 
   useEffect(() => {
     firebaseAuth.auth().onAuthStateChanged(setCurrentUser);
-    console.log(currentUser);
+    //console.log(currentUser);
   }, [currentUser]);
 
   return (
